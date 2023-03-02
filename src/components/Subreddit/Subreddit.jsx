b44fec7278503417938b8b21e57b7d3037a9c239
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-03-01 22:14:23 -0500

    component Subreddits

---
+++ src/components/Subreddit/Subreddit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Subreddit.css";
+const Subreddit = () => {
+  return (
+    <div className="subreddit">
+      <img></img>
+      <h3 className="subreddit-name">Home</h3>
+    </div>
+  );
+};
+
+export default Subreddit;
--- src/components/Subreddits/Subreddits.jsx
+++ src/components/Subreddits/Subreddits.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Subreddit from "../Subreddit/Subreddit";
+import "./Subreddits.css";
 
 const Subreddits = () => {
-  return <div>Subreddits</div>;
+  return (
+    <div className="subreddits">
+      <h2>Subreddits</h2>
+      <div className="subreddit-single">
+        <Subreddit />
+        <Subreddit />
+        <Subreddit />
+      </div>
+    </div>
+  );
 };
 
 export default Subreddits;
