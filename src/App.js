5803ae4d292ba171f71e5e54d866b444c77453de
Carlos Requena <101939628+carlos-req@users.noreply.github.com>
2023-03-01 20:59:14 -0500

    add main post component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
+import MainPost from "./components/MainPost/MainPost";
 import NavBar from "./components/NavBar/NavBar";
+import Subreddits from "./components/Subreddits/Subreddits";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <div className="components">
+        <div className="MainPost">
+          <MainPost />
+        </div>
+        <div className="Subreddits">
+          <Subreddits />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/PostCard/PostCard.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { FaRegCommentAlt } from "react-icons/fa";
+import { BiUpArrow, BiDownArrow } from "react-icons/bi";
+import "./PostCard.css";
+const PostCard = () => {
+  return (
+    <div className="post-card">
+      <div className="vote-post">
+        <BiUpArrow className="arrows" />
+        <p className="rating">62.9k</p>
+        <BiDownArrow className="arrows" />
+      </div>
+      <div className="main-post">
+        <h3 className="post-heading">This will be the heading of the post</h3>
+        <img></img>
+        <hr />
+        <div className="bottom-post">
+          <p className="user">flyingcatwithhorns</p>
+          <p>10 hours ago</p>
+          <div className="comments-count">
+            <FaRegCommentAlt />
+            <p>5.8k</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostCard;
